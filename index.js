d4ccb80ab7a4e549e14fea8fa3045404d977af33
Brent Houston <brentjustinhouston@gmail.com>
2023-04-23 14:47:20 -0700

    passes all jest tests

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ const fs = require('fs');
 const generateHtml = require('./src/generateHtml.js');
 const Employee = require('./lib/Employee.js');
 const Manager = require('./lib/Manager.js');
-const Intern = require('./lib/Intern.js')
+const Intern = require('./lib/Intern.js');
+const Engineer = require('./lib/Engineer.js');
+
+
 
 const teamMem =[];
 
@@ -29,9 +32,7 @@ function manager(){
 ]).then((ans) =>{
         const myMgr = new Manager(ans.name,ans.id,ans.email,ans.officeNumber);
         teamMem.push(myMgr);
-        console.log(teamMem)
-        // instance of the manager
-    //put it in the team array
+        menu();
 })
 }
 // another function that handles menu
@@ -42,64 +43,102 @@ function menu(){
             type: 'list',
             name: 'menu',
             message: 'What would you like to do next?',
-            chioces: ["Add Engineer", "Add Intern","Finish bulding my team"],
+            choices: ["Add Engineer", "Add Intern","Finish bulding my team"],
         },
     ]).then((ans) => {
-        //call the right function,engineer ect.
-    })
-}
+        switch (ans.menu) {
+            case 'Add Engineer': engineer();
+                
+                break;
+
+            case 'Add Intern': intern();
+                
+                break;
+        
+        
+            default:
+                finish();
+                break;
+            }
+            
+        })
+    }
+
+
 function engineer(){
-    inquirer.prompt([
-    {
-        type: 'input',
-        name: 'name',
-        message: 'What is the team managers name?',
-    },
-    {
-        type: 'input',
-        name: 'id',
-        message: 'What is the team managers id?',
-    },
-    {
-        type: 'input',
-        name: 'email',
-        message: 'What is the team managers email?',
+        inquirer.prompt([
+            {
+                type: 'input',
+                name: 'name',
+                message: 'What is the Engineers name?',
+            },
+            {
+                type: 'input',
+                name: 'id',
+                message: 'What is the Engineers id?',
+            },
+            {
+                type: 'input',
+                name: 'email',
+                message: 'What is the Engineers email?',
+            },
+            {
+                type: 'input',
+                name: 'github',
+                message: 'What is the Engineers GitHub username?',
+            }
+            
+        ]).then((ans) =>{ 
+            const myEng = new Engineer(ans.name,ans.id,ans.email,ans.github);
+            teamMem.push(myEng);
+            menu();
+            
+        })
     }
-    
-]).then((ans) =>{
-    // instance of the manager
-    //put it in the team array
-})
-}
+
+
 function intern(){
-    inquirer.prompt([
-    {
-        type: 'input',
-        name: 'name',
-        message: 'What is the team managers name?',
-    },
-    {
-        type: 'input',
-        name: 'id',
-        message: 'What is the team managers id?',
-    },
-    {
-        type: 'input',
-        name: 'email',
-        message: 'What is the team managers email?',
+        inquirer.prompt([
+            {
+                type: 'input',
+                name: 'name',
+                message: 'What is the Inters name?',
+            },
+            {
+                type: 'input',
+                name: 'id',
+                message: 'What is the Inters id?',
+            },
+            {
+                type: 'input',
+                name: 'email',
+                message: 'What is the Inters email?',
+            },
+            {
+                type: 'input',
+                name: 'school',
+                message: 'What school is the Inter coming from?',
+            }
+            
+        ]).then((ans) =>{
+            const myInt = new Intern(ans.name,ans.id,ans.email,ans.school);
+            teamMem.push(myInt);
+            menu();
+            console.log(teamMem);
+            
+        })
     }
     
-]).then((ans) =>{
-    const myInt = new Intern(ans.name,ans.id,ans.email,ans.school);
-    teamMem.push(ans.myInt);
-    // instance of the Intern
-    //put it in the team array
-})
-}
+    function finish(){
+        
+    fs.writeFile("index.html", generateHtml, (err) =>
+    err ? console.log(err) : console.log("Success your team has been built!")
+  );
 
-function finish(){
-    //fs write
-    //console sucess or error
-}
+        
+       
+    }
+    
+    manager();
+    
 
-manager();
