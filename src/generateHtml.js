6fbd70885f515e25b3e4535bb3a92a154ac047f3
Brent Houston <brentjustinhouston@gmail.com>
2023-04-23 22:07:33 -0700

    added CSS

--- src/generateHtml.js
+++ src/generateHtml.js
@@ -95,6 +95,8 @@ module.exports = team => {
         integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="stylesheet" href="style.css">
     <script src="https://kit.fontawesome.com/c502137733.js"></script>
+    <link rel="stylesheet" href="https://use.typekit.net/bfj3agg.css">
+    
 </head>
 
 <body>
